b0d6bb168174ef2265d7ad492679710a94146a3f
hindustantech <kumarrupees507@gmail.com>
2025-09-09 15:59:36 +0530

    ad

--- routes/BannerRoute.js
+++ routes/BannerRoute.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import { createBanner } from '../controllers/BannerController.js';
-
+import multer from 'multer';
 const router = express.Router();
 
-router.post('/createbanner', createBanner);
+const storage = multer.memoryStorage(); // ✅ stores buffer in memory
+
+const upload = multer({ storage });
+
+
+router.post('/createbanner', upload.single("images"), createBanner);
 
 export default router;
