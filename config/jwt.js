c9daeea1f60757a580c76b9da04344d2d28c00f7
hindustantech <kumarrupees507@gmail.com>
2025-09-17 17:16:47 +0530

    ad

--- config/jwt.js
+++ config/jwt.js
@@ -1,7 +1,6 @@
 // const jwt = require('jsonwebtoken');
 import jwt from 'jsonwebtoken';
 // utils/jwt.js
-import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 // const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '7d';
