8ad21d07dff9c23237502534153c5aac4dc0e012
hindustantech <kumarrupees505@gmail.com>
2025-10-04 13:05:06 +0530

    add

--- routes/promotionalRoute.js
+++ routes/promotionalRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getPromotionalBanners, getAllPromotionalBanners, createPromotionalBanner, updatePromotionalBanner, deletePromotionalBanner, toggleBannerStatus } from "../controllers/promotionalBannerController.js"; 
-import authMiddleware from "../middlewares/authMiddleware.js";
+import { authMiddleware1 } from "../middlewares/checkuser.js";
 const router = express.Router();
 
 router.post("/", createPromotionalBanner);
@@ -8,6 +8,6 @@ router.put("/:id", updatePromotionalBanner);
 router.delete("/:id", deletePromotionalBanner);
 router.patch("/:id/status", toggleBannerStatus);
 router.get("/", getAllPromotionalBanners); // <-- Fetch with filters
-router.get('/my-banners', authMiddleware, getPromotionalBanners);
+router.get('/my-banners', authMiddleware1, getPromotionalBanners);
 
 export default router;
