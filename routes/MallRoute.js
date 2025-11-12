17450eedadf88c8cdb99bbce19116899983add87
hindustantech <kumarrupees505@gmail.com>
2025-11-12 14:22:31 +0530

    add

--- routes/MallRoute.js
+++ routes/MallRoute.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getMallsWithUserLocation, createOrUpdateMall, getMallshop } from '../controllers/MallController.js';
-
+import authMiddleware from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 // Endpoint: GET /api/malls/getMalls
-router.get('/getMalls', getMallsWithUserLocation);
-router.get('/getMalls', createOrUpdateMall);
-router.get('/getMalls', getMallshop);
+router.get('/getMalls', authMiddleware ,getMallsWithUserLocation);
+router.post('/createOrUpdateMall', createOrUpdateMall);
+router.get('/getMallshop', getMallshop);
 
 export default router;
