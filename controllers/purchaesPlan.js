c52e89b4d52b3b000b64417b06e32f749c086550
hindustantech <kumarrupees505@gmail.com>
2025-12-26 15:57:14 +0530

    add

--- controllers/purchaesPlan.js
+++ controllers/purchaesPlan.js
@@ -1,9 +1,10 @@
 import Banner from "../models/Banner.js";
 import Plan from "../models/Plan.js";
-import Coupon from "../models/Coupon.js";
+import Coupon from "../models/coupunModel.js";
 import TopUpAttempt from "../models/TopUpAttempt.js";
 import Transaction from "../models/Transaction.js";
 import Wallet from "../models/Wallet.js";
+import UserPlan from "../models/UserPlan.js";
 
 /* ============================
     UPDATE PLAN
