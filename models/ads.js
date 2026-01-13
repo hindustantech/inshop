adbc04c6e3dbdde8def441ff28d795301f4e84a4
hindustantech <kumarrupees505@gmail.com>
2026-01-13 12:09:05 +0530

    spa

--- models/ads.js
+++ models/ads.js
@@ -3,8 +3,14 @@ import mongoose from 'mongoose';
 const adSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,  
+        required: true,
         trim: true
+    },
+    activity_use: {
+        type: String,
+        require: true,
+        enum: ['activity', 'advertisament','unlock'],
+        default: "activity"
     },
     desc: {
         type: String,
