2c1f40917c8ebb687d4884d54e4b5428ac1950d1
hindustantech <kumarrupees505@gmail.com>
2026-01-28 17:34:37 +0530

    add coperet user

--- routes/corperate.js
+++ routes/corperate.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { createCorporateRequest, updateCorporateRequestStatus, getCorporateRequestById, getAllCorporateRequests, getMyCorporateRequests } from '../controllers/corparete.js';
+import {
+    createCorporateRequest,
+    updateCorporateRequestStatus,
+    getCorporateRequestById,
+    getAllCorporateRequests,
+    getMyCorporateRequests,
+    toggleCorporateUser
+} from '../controllers/corparete.js';
 const router = express.Router();
 
 
 router.post("/corporate-request", authMiddleware, createCorporateRequest);
 
+router.patch(
+    "/users/:userId/corporate",
+    authMiddleware,
+    toggleCorporateUser       
+);
 router.patch("/corporate-request/:requestId/status", authMiddleware, updateCorporateRequestStatus);
 
 router.get("/corporate-request/:requestId", authMiddleware, getCorporateRequestById);
